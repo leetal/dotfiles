77d85754ed4168d8b71e5c1e4c22117029164912
Alexander Widerberg <widerbergaren@gmail.com>
2015-05-15 01:25:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,20 +15,11 @@ cd
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 ## Append to our .zshrc file written by oh-my-zsh
-echo "
-
-# Enable vi-mode
-bindkey -v
-export KEYTIMEOUT=1
-# Add some PATH variables
-export PATH=~/.bin/:/usr/local/bin/:$PATH
-# Sourcing from other files
-source $HOME/.zsh/aliases
-source $HOME/.zsh/functions
-source $HOME/.zsh/prompt
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >> ~/.zshrc
+cat "$DIR/.zshrc" >> ~/.zshrc
 
 popd
+
+set -e
 
 echo "\033[0;32m"' _____          _        _ _          _ '"\033[0m"
 echo "\033[0;32m"'|_   _|        | |      | | |        | |'"\033[0m"
