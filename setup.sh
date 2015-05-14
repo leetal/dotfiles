b7a7a89ce070cd118af0d65d59f1d8579925695c
Alexander Widerberg <widerbergaren@gmail.com>
2015-05-14 09:44:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,6 +12,18 @@ rsync -av --progress --exclude-from 'exclude-list.txt' .* ~/
 ## Install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
+## Append to our .zshrc file written by oh-my-zsh
+echo "\n\n# Enable vi-mode\n
+bindkey -v\n
+export KEYTIMEOUT=1\n
+# Add some PATH variables\n
+export PATH=~/.bin/:/usr/local/bin/:$PATH\n
+# Sourcing from other files\n
+source $HOME/.zsh/aliases\n
+source $HOME/.zsh/functions\n
+source $HOME/.zsh/prompt\n
+export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting" >> ~/.zshrc
+
 ## Restart everything!
 ScriptLoc=$(readlink -f "$0")
 exec $ScriptLoc
