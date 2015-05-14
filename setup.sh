c73c2f21b525a3f5ac1f5d8cd022103ff46269aa
Alexander Widerberg <widerbergaren@gmail.com>
2015-05-14 09:41:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+## Jump to home
+cd
+
+## First make sure we have installed zsh and curl!
+sudo apt-get -y --force-yes install zsh curl
+
 ## This will setup all the dotfiles where they belong!
-rsync -av --progress --exclude-from 'exclude-list.txt' .* ~/ 
+rsync -av --progress --exclude-from 'exclude-list.txt' .* ~/
+
+## Install oh-my-zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+## Restart everything!
+ScriptLoc=$(readlink -f "$0")
+exec $ScriptLoc
